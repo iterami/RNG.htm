d104a073d9d6dfecc101b81619eb62d0e54a106a
Jan Horava <czechinthemail@gmail.com>
2013-05-02 23:34:26 -0700

    Cleaned Up Code

--- rng.js
+++ rng.js
@@ -1,9 +1,12 @@
+function get(i){
+    return document.getElementById(i)
+}
 function random_number(){
     save();
-    i=value('repeat')-1;
-    range=parseInt(value('range'))+1;
+    i=get('repeat').value-1;
+    range=parseInt(get('range').value)+1;
     do{
-        result+=Math.floor(Math.random()*range+value('base'))+' '
+        result+=Math.floor(Math.random()*range+get('base').value)+' '
     }while(i--);
     get('result').innerHTML=result;
     result=''
@@ -18,31 +21,25 @@ function reset(){
     }
 }
 function save(){
-    if(value('repeat')<1){
+    if(get('repeat').value<1){
         get('repeat').value=1
     }
     i=2;
     j=['base','range','repeat'];
     do{
-        if(isNaN(value(j[i]))||value(j[i])==[0,10,1][i]){
+        if(isNaN(get(j[i]).value)||get(j[i]).value==[0,10,1][i]){
             get(j[i]).value=[0,10,1][i];
             ls.removeItem('rng'+i)
         }else{
-            ls.setItem('rng'+i,value(j[i]))
+            ls.setItem('rng'+i,get(j[i]).value)
         }
     }while(i--);
     j=''
 }
-function get(i){
-    return document.getElementById(i)
-}
-function value(i){
-    return get(i).value
-}
 
 var i=range=2,
-ls=window.localStorage,
-j=result='';
+j=result='',
+ls=window.localStorage;
 
 get('base').value=ls.getItem('rng0')==null?0:ls.getItem('rng0');
 get('range').value=ls.getItem('rng1')==null?10:ls.getItem('rng1');
@@ -51,7 +48,7 @@ get('repeat').value=ls.getItem('rng2')==null?1:ls.getItem('rng2');
 window.onkeydown=function(e){
     i=window.event?event:e;
     i=i.charCode?i.charCode:i.keyCode;
-    if(i==72){
+    if(i==72){/*H*/
         random_number()
     }
 }
