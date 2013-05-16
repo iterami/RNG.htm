c3a117663c89e9e84f9209db2c2102e186b46c04
Jan Horava <czechinthemail@gmail.com>
2013-05-15 20:30:29 -0700

    improved formatting

--- rng.js
+++ rng.js
@@ -3,52 +3,54 @@ function get(i){
 }
 function random_number(){
     save();
-    i=get('repeat').value-1;
-    range=parseInt(get('range').value)+1;
+    i = get('repeat').value-1;
+    range = parseInt(get('range').value)+1;
     do{
-        result+=Math.floor(Math.random()*range+get('base').value)+' '
+        result += Math.floor(Math.random()*range+get('base').value)+' '
     }while(i--);
-    get('result').innerHTML=result;
-    result=''
+    get('result').innerHTML = result;
+    result = ''
 }
 function reset(){
     if(confirm('Reset settings?')){
-        get('base').value=0;
-        get('range').value=10;
-        get('repeat').value=1;
-        get('result').innerHTML='';
+        get('base').value = 0;
+        get('range').value = 10;
+        get('repeat').value = 1;
+        get('result').innerHTML = '';
         save()
     }
 }
 function save(){
     if(get('repeat').value<1){
-        get('repeat').value=1
+        get('repeat').value = 1
     }
-    i=2;
-    j=['base','range','repeat'];
+    i = 2;
+    j = ['base','range','repeat'];
     do{
-        if(isNaN(get(j[i]).value)||get(j[i]).value==[0,10,1][i]){
-            get(j[i]).value=[0,10,1][i];
+        if(isNaN(get(j[i]).value) || get(j[i]).value===[0,10,1][i]){
+            get(j[i]).value = [0,10,1][i];
             ls.removeItem('rng'+i)
         }else{
             ls.setItem('rng'+i,get(j[i]).value)
         }
     }while(i--);
-    j=''
+    j = ''
 }
 
-var i=range=2,
-j=result='',
-ls=window.localStorage;
+var i = 0;
+var j = '';
+var ls = window.localStorage;
+var range = 2;
+var result = '';
 
-get('base').value=ls.getItem('rng0')==null?0:ls.getItem('rng0');
-get('range').value=ls.getItem('rng1')==null?10:ls.getItem('rng1');
-get('repeat').value=ls.getItem('rng2')==null?1:ls.getItem('rng2');
+get('base').value = ls.getItem('rng0')===null ? 0 : ls.getItem('rng0');
+get('range').value = ls.getItem('rng1')===null ? 10 : ls.getItem('rng1');
+get('repeat').value = ls.getItem('rng2')===null ? 1 : ls.getItem('rng2');
 
-window.onkeydown=function(e){
-    i=window.event?event:e;
-    i=i.charCode?i.charCode:i.keyCode;
-    if(i==72){/*H*/
+window.onkeydown = function(e){
+    i = window.event ? event : e;
+    i = i.charCode ? i.charCode: i.keyCode;
+    if(i===72){/*H*/
         random_number()
     }
 }
