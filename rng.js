75db3323651115f159041370a0a2a6f1c74ac6a5
Jan Horava <czechinthemail@gmail.com>
2013-05-20 16:25:03 -0700

    improved formatting

--- rng.js
+++ rng.js
@@ -1,16 +1,18 @@
 function get(i){
     return document.getElementById(i)
 }
+
 function random_number(){
     save();
-    i = get('repeat').value-1;
-    range = parseInt(get('range').value)+1;
+    i = get('repeat').value - 1;
+    range = parseInt(get('range').value) + 1;
     do{
-        result += Math.floor(Math.random()*range+get('base').value)+' '
+        result += Math.floor(Math.random() * range + get('base').value) + ' '
     }while(i--);
     get('result').innerHTML = result;
     result = ''
 }
+
 function reset(){
     if(confirm('Reset settings?')){
         get('base').value = 0;
@@ -20,18 +22,19 @@ function reset(){
         save()
     }
 }
+
 function save(){
-    if(get('repeat').value<1){
+    if(get('repeat').value < 1){
         get('repeat').value = 1
     }
     i = 2;
     j = ['base','range','repeat'];
     do{
-        if(isNaN(get(j[i]).value) || get(j[i]).value===[0,10,1][i]){
+        if(isNaN(get(j[i]).value) || get(j[i]).value === [0,10,1][i]){
             get(j[i]).value = [0,10,1][i];
-            ls.removeItem('rng'+i)
+            ls.removeItem('rng' + i)
         }else{
-            ls.setItem('rng'+i,get(j[i]).value)
+            ls.setItem('rng' + i,get(j[i]).value)
         }
     }while(i--);
     j = ''
@@ -43,14 +46,14 @@ var ls = window.localStorage;
 var range = 2;
 var result = '';
 
-get('base').value = ls.getItem('rng0')===null ? 0 : ls.getItem('rng0');
-get('range').value = ls.getItem('rng1')===null ? 10 : ls.getItem('rng1');
-get('repeat').value = ls.getItem('rng2')===null ? 1 : ls.getItem('rng2');
+get('base').value = ls.getItem('rng0') === null ? 0 : ls.getItem('rng0');
+get('range').value = ls.getItem('rng1') === null ? 10 : ls.getItem('rng1');
+get('repeat').value = ls.getItem('rng2') === null ? 1 : ls.getItem('rng2');
 
 window.onkeydown = function(e){
     i = window.event ? event : e;
     i = i.charCode ? i.charCode: i.keyCode;
-    if(i===72){/*H*/
+    if(i === 72){/*H*/
         random_number()
     }
 }
