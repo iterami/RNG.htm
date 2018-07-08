fee11d1c031701730412c1150dc036e2b3223bf8
Jan Hořava <czechinthemail@gmail.com>
2018-07-07 17:06:41 -0700

    changed var to let

--- js/data.js
+++ js/data.js
@@ -3,11 +3,11 @@
 function generate(){
     core_storage_save();
 
-    var result = '';
+    let result = '';
 
     // Generate random number(s).
-    var loop_counter = core_storage_data['repeat'] - 1;
-    var range = core_storage_data['range'] + 1;
+    let loop_counter = core_storage_data['repeat'] - 1;
+    let range = core_storage_data['range'] + 1;
     do{
         result += core_random_integer({
           'max': range,
