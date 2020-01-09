ccacc626410b2d432d027628cd7d441712f152d5
Jan Hořava <czechinthemail@gmail.com>
2020-01-08 20:29:41 -0800

    added optional core_random_crypto() usage

--- js/data.js
+++ js/data.js
@@ -9,9 +9,17 @@ function generate(){
     let loop_counter = core_storage_data['repeat'] - 1;
     let range = core_storage_data['range'] + 1;
     do{
-        result += core_random_integer({
-          'max': range,
-        }) + core_storage_data['base'];
+        if(core_storage_data['crypto']){
+console.log(1);
+            result += Math.floor((core_random_crypto({
+              'type': 'Uint32Array',
+            }) / 4294967295) * range);
+
+        }else{
+            result += core_random_integer({
+              'max': range,
+            }) + core_storage_data['base'];
+        }
 
         if(loop_counter > 0){
             result += core_storage_data['separator'];
--- js/main.js
+++ js/main.js
@@ -17,11 +17,13 @@ function repo_init(){
       'menu-block-events': false,
       'storage': {
         'base': 0,
+        'crypto': false,
         'range': 10,
         'repeat': 1,
         'separator': ', ',
       },
       'storage-menu': '<table><tr><td><input id=base><td>Base'
+        + '<tr><td><input id=crypto type=checkbox><td>Crypto'
         + '<tr><td><input id=range><td>Range'
         + '<tr><td><input id=repeat><td>Repeat'
         + '<tr><td><input id=separator><td>Separator</table>',
