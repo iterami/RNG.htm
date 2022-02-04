833913a7d0efef1ea63f4ae3b5b21e2fce713cf8
Jan Hořava <czechinthemail@gmail.com>
2022-02-03 18:13:44 -0800

    added mini class to storage inputs

--- js/main.js
+++ js/main.js
@@ -22,11 +22,11 @@ function repo_init(){
         'repeat': 1,
         'separator': ', ',
       },
-      'storage-menu': '<table><tr><td><input id=base step=any type=number><td>Base'
+      'storage-menu': '<table><tr><td><input class=mini id=base step=any type=number><td>Base'
         + '<tr><td><input id=crypto type=checkbox><td>Crypto'
-        + '<tr><td><input id=range step=any type=number><td>Range'
-        + '<tr><td><input id=repeat min=1 step=any type=number><td>Repeat'
-        + '<tr><td><input id=separator><td>Separator</table>',
+        + '<tr><td><input class=mini id=range step=any type=number><td>Range'
+        + '<tr><td><input class=mini id=repeat min=1 step=any type=number><td>Repeat'
+        + '<tr><td><input class=mini id=separator><td>Separator</table>',
       'title': 'RNG.htm',
     });
 
